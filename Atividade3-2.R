477f324e379fb4e9cd7e8edf2206e26a74f3e294
tailine <tailine.juliana@gmail.com>
2021-03-24 15:17:47 -0300

    Reorganização

---
+++ Atividade3-2.R
@@ -0,0 +1,12 @@
+####################################################
+#                 TAILINE NONATO                   #
+#                  ATIVIDADE 3.2                   #
+####################################################
+
+
+# Carregando pacotes ----
+if (!require(pacman)) {
+  install.packIdades("pacman")
+  library(pacman)}
+
+pacman::p_load(tidyverse,dplyr)
---
+++ Atividade3-3.R
@@ -0,0 +1,12 @@
+####################################################
+#                 TAILINE NONATO                   #
+#                  ATIVIDADE 3.3                   #
+####################################################
+
+
+# Carregando pacotes ----
+if (!require(pacman)) {
+  install.packIdades("pacman")
+  library(pacman)}
+
+pacman::p_load(tidyverse,dplyr)
