c0d658abffa53f624e49dab54d10b41abe8c3fb7
tailine <56554634+tailineju@users.noreply.github.com>
2021-04-19 12:05:25 -0300

    Update

--- Atividade4-2.R
+++ Atividade4-2.R
@@ -9,9 +9,53 @@ if (!require(pacman)) {
   install.packIdades("pacman")
   library(pacman)}
 
-pacman::p_load(tidyverse,dplyr)
+pacman::p_load(tidyverse)
+
+theme.t <- function(position_legend = "top"){
+  return(list(
+    theme_bw(),
+    theme(axis.title.y=element_text(colour="black", size=12),
+          axis.title.x = element_text(colour="black", size=12),
+          axis.text = element_text(colour = "black", size=9.5),
+          panel.border = element_blank(),
+          axis.line = element_line(colour = "black")),
+    theme(legend.position=position_legend)))}
 
 # Dados ----
 
-dados <- read.csv("Outros arquivos/amostra.csv", encoding = "UTF-8", 
+a.100 <- read.csv("Outros arquivos/tam100.csv", encoding = "UTF-8", 
                   na.strings=c(" ","NA"))
+a.30 <- read.csv("Outros arquivos/tam30.csv", encoding = "UTF-8", 
+                  na.strings=c(" ","NA"))
+
+# Amostra tam.100 ----
+
+# NOTA_MT e LOCALIZAÇÃO
+# Gráficos 
+
+# Resumo
+
+# Testes Associação
+
+# NOTA_LP e DEPENDENCIA_ADM
+# Gráficos 
+
+# Resumo
+
+# Testes Associação
+
+# Amostra tam.30 ----
+
+# NOTA_MT e LOCALIZAÇÃO
+# Gráficos 
+
+# Resumo
+
+# Testes Associação
+
+# NOTA_LP e DEPENDENCIA_ADM
+# Gráficos 
+
+# Resumo
+
+# Testes Associação
